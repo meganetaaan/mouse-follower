4fedad81894ba4e9b64331b924a06b33ad7991e9
Shinya ISHIKAWA <ishikawa.s.1027@gmail.com>
2025-08-08 15:00:24 +0900

    Add build:lib settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true' 
     ? '/mouse-follower/' 
     : '/',
+  build: {
+    outDir: 'dist-demo'
+  }
 })
